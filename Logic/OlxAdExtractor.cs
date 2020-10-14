0b1c9dc20f26f3e6858c9e26b9c8224f883ec662
Radosław  Czebreszuk <radek.czebreszuk@gmail.com>
2020-10-14 21:24:15 +0200

    add data extractor for Olx Ad

---
+++ Logic/OlxAdExtractor.cs
@@ -0,0 +1,36 @@
+namespace Radz1u.Logic {
+    using System.Collections.Generic;
+    using System;
+    using HtmlAgilityPack;
+
+    public class OlxAdExtractor {
+        private OlxDateParser _olxDateParser;
+
+        public OlxAdExtractor () {
+            _olxDateParser = new OlxDateParser ();
+        }
+
+        public IEnumerable<OlxAdContract> Extract (string content) {
+            var htmlDocument = new HtmlDocument ();
+            htmlDocument.LoadHtml (content);
+            var offers = htmlDocument.DocumentNode.SelectNodes ("//td[@class='offer']");
+            foreach (var offer in offers) {
+                yield return new OlxAdContract {
+                    PublishDate = ExtractPublishDate (offer),
+                        Url = ExtractAdUrl(offer)
+                };
+            }
+        }
+
+        private DateTime ExtractPublishDate (HtmlNode offer) {
+            var clockData = offer.SelectNodes ("//i[@data-icon='clock']") [0];
+            var dateTime = clockData.InnerText;
+            return _olxDateParser.Parse (dateTime);
+        }
+
+        private string ExtractAdUrl (HtmlNode offer) {
+            var linkData = offer.SelectNodes ("//a") [0];
+            return linkData.GetAttributeValue ("href", "");
+        }
+    }
+}
