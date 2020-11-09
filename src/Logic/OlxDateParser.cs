e316ed3b3f5ea8040e7a32a724799829ae320ee1
Radosław  Czebreszuk <radek.czebreszuk@gmail.com>
2020-11-09 21:04:58 +0100

    Fix date parser bug with invalid format - double spaces

--- src/Logic/OlxDateParser.cs
+++ src/Logic/OlxDateParser.cs
@@ -19,22 +19,31 @@ namespace Radz1u.Logic {
                 return DateTime.Now;
             }
 
+            var formattedDate = FormatDate(date); 
+
             if (date.Contains (_todayPrefix)) {
                 return ParseToday (date);
             } else if (date.Contains (_yesterdayPrefix)) {
                 return ParseYesterday (date);
             }
 
-            return DateTime.ParseExact (date, "dd MMM",_dateTimeCulture);
+            return DateTime.ParseExact (formattedDate, "dd MMM",_dateTimeCulture);
         }
 
-        public DateTime ParseToday (string date) {
+        ///There is sometimes double spaces between day and month
+        private string FormatDate(string date)
+        {
+            var splittedWords = date.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+            return String.Join(" ", splittedWords);
+        }
+
+        private DateTime ParseToday (string date) {
             var today = date.Replace (_todayPrefix, DateTime.Today.ToString ("dd MMM", _dateTimeCulture));
 
             return DateTime.ParseExact (today, "dd MMM HH:mm", _dateTimeCulture);
         }
 
-        public DateTime ParseYesterday (string date) {
+        private DateTime ParseYesterday (string date) {
             var yesterdayDateTime = DateTime.Today.AddDays (-1);
             var yesterday = date.Replace (_yesterdayPrefix, yesterdayDateTime.ToString ("dd MMM", _dateTimeCulture));
             return DateTime.ParseExact (yesterday, "dd MMM HH:mm", _dateTimeCulture);
